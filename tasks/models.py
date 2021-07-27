4f54e81f9aebe9d561f8f75f76f6067fdea1ad9a
Iphone6sisgood <sudarshan@testpress.in>
2021-07-27 12:28:46 +0530

    create project form

---
+++ tasks/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from model_utils.models import TimeFramedModel
+from taggit.managers import TaggableManager
+
+# Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(TimeFramedModel):
+    text = models.CharField(max_length=40)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    tag = TaggableManager()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
